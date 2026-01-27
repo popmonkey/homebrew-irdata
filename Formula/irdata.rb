480d4827c7046197c4a87341ac1adcbc184a1155
popmonkey <popmonkey@users.noreply.github.com>
2026-01-26 23:07:49 -0800

    Update irdata.rb - v0.6.4

--- Formula/irdata.rb
+++ Formula/irdata.rb
@@ -1,10 +1,10 @@
 class Irdata < Formula
   desc "github.com/popmonkey/irdata tools"
   homepage "https://github.com/popmonkey/irdata"
-  version "0.6.3"
+  version "0.6.4"
 
-  url "https://github.com/popmonkey/irdata/releases/download/v0.6.3/irfetch-osx.zip"
-  sha256 "fad16e97e432e546716904e6738fffc2363197693295cad1a5f81f83aaf2aeb0"
+  url "https://github.com/popmonkey/irdata/releases/download/v0.6.4/irfetch-osx.zip"
+  sha256 "2bb871988d62db9bc6964650e395acc6c5623101073749e668ffb403c49b33e8"
 
   def install
     bin.install "irfetch.osx-universal" => "irfetch"
